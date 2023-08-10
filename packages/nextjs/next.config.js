725435f98b35903eb1c3eab3c02aa0cf0f00dca9
fleekgonzalo <114927947+fleekgonzalo@users.noreply.github.com>
2023-08-10 13:44:20 -0300

    Update next.config.js

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,6 +3,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
